ed8a55f1bfe8a52448b55f9c4a25bc57ef8ec558
Saydulayev <saydulayev.wien@gmail.com>
2023-09-18 13:09:13 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Akhmed on 16.09.23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
