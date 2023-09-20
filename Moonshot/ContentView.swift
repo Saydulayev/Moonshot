6cddedfd26bba3177e232cafc0669fe03fcbda8b
Saydulayev <saydulayev.wien@gmail.com>
2023-09-20 12:11:25 +0200

    - Added the launch date information below the mission badge in MissionView - Extracted the horizontal scroll view and rectangle dividers into separate SwiftUI views for better modularity - Introduced a toolbar item in ContentView to toggle between grid and list views for missions

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showSplash = true
-    
+    @State private var isGridView = true // New state variable for toggling between grid and list
     
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
@@ -22,63 +22,86 @@ struct ContentView: View {
         ZStack {
             NavigationView {
                 ScrollView {
-                    LazyVGrid(columns: columns) {
-                        ForEach(missions) { mission in
-                            NavigationLink {
-                                MissionView(mission: mission, astronauts: astronauts)
-                            } label: {
-                                VStack {
-                                    Image(mission.image)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 100, height: 100)
-                                        .padding()
-                                        .background(RoundedRectangle(cornerRadius: 30)
-                                            .fill(.darkBackground))
-                                        .shadow(color: Color.white.opacity(0.3), radius: 3, x: 5, y: 5)
-                                        .padding()
-                                    VStack {
-                                        Text(mission.displayName)
-                                            .font(.headline)
-                                            .foregroundColor(.white)
-                                        Text(mission.formattedLaunchDate)
-                                            .font(.caption)
-                                            .foregroundColor(.white.opacity(0.5))
-                                    }
-                                    .padding(.vertical)
-                                    .frame(maxWidth: .infinity)
-                                    .background(.lightBackground)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(Color.cyan.opacity(0.5))
-                                    )
-                                }
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(.lightBackground)
-                                )
-                            }
+                    if isGridView {
+                        LazyVGrid(columns: columns) {
+                            missionGrid
+                        }
+                        .padding([.horizontal, .bottom])
+                    } else {
+                        VStack {
+                            missionGrid
+                            
                         }
                     }
-                    .padding([.horizontal, .bottom])
+                }
+                .toolbar { // New toolbar item to toggle the display
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            withAnimation {
+                                isGridView.toggle()
+                            }
+                        }) {
+                            Image(systemName: isGridView ? "list.dash" : "square.grid.2x2")
+                        }
+                    }
                 }
                 .navigationTitle("Moonshot")
-                .background(.darkBackground)
+                .background(Color.darkBackground)
                 .preferredColorScheme(.dark)
             }
-            if showSplash {
-                AnimatedImage(imageName: "astronaut1")
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .background(Color.black)
-                    .edgesIgnoringSafeArea(.all)
-            }
-        } 
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                withAnimation {
-                    showSplash = false
+//            if showSplash {
+//                AnimatedImage(imageName: "astronaut1")
+//                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                    .background(Color.darkBackground)
+//                    .edgesIgnoringSafeArea(.all)
+//            }
+        }
+//        .onAppear {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                withAnimation {
+//                    showSplash = false
+//                }
+//            }
+//        }
+    }
+    
+    
+    var missionGrid: some View {
+        ForEach(missions) { mission in
+            NavigationLink {
+                MissionView(mission: mission, astronauts: astronauts)
+            } label: {
+                VStack {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 30)
+                                        .fill(Color.darkBackground))
+                        .shadow(color: Color.white.opacity(0.3), radius: 3, x: 5, y: 5)
+                        .padding()
+                    VStack {
+                        Text(mission.displayName)
+                            .font(.headline)
+                            .foregroundColor(.white)
+                        Text(mission.formattedLaunchDate)
+                            .font(.caption)
+                            .foregroundColor(.white.opacity(0.5))
+                    }
+                    .padding(.vertical)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.lightBackground)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.cyan.opacity(0.5))
+                    )
                 }
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.lightBackground)
+                )
             }
         }
     }
@@ -86,16 +109,86 @@ struct ContentView: View {
 
 
 
+// Old Code...
 
-
-//struct SplashScreen: View {
+//struct ContentView: View {
+//    @State private var showSplash = true
+//
+//
+//    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+//    let missions: [Mission] = Bundle.main.decode("missions.json")
+//
+//    let columns = [
+//        GridItem(.adaptive(minimum: 150))
+//    ]
+//
 //    var body: some View {
-//        AnimatedImage(imageName: "astronaut1")
-//            .frame(maxWidth: .infinity, maxHeight: .infinity)
-//            .background(Color.blue)
-//            .edgesIgnoringSafeArea(.all)
+//        ZStack {
+//            NavigationView {
+//                ScrollView {
+//                    LazyVGrid(columns: columns) {
+//                        ForEach(missions) { mission in
+//                            NavigationLink {
+//                                MissionView(mission: mission, astronauts: astronauts)
+//                            } label: {
+//                                VStack {
+//                                    Image(mission.image)
+//                                        .resizable()
+//                                        .scaledToFit()
+//                                        .frame(width: 100, height: 100)
+//                                        .padding()
+//                                        .background(RoundedRectangle(cornerRadius: 30)
+//                                            .fill(.darkBackground))
+//                                        .shadow(color: Color.white.opacity(0.3), radius: 3, x: 5, y: 5)
+//                                        .padding()
+//                                    VStack {
+//                                        Text(mission.displayName)
+//                                            .font(.headline)
+//                                            .foregroundColor(.white)
+//                                        Text(mission.formattedLaunchDate)
+//                                            .font(.caption)
+//                                            .foregroundColor(.white.opacity(0.5))
+//                                    }
+//                                    .padding(.vertical)
+//                                    .frame(maxWidth: .infinity)
+//                                    .background(.lightBackground)
+//                                    .overlay(
+//                                        RoundedRectangle(cornerRadius: 10)
+//                                            .stroke(Color.cyan.opacity(0.5))
+//                                    )
+//                                }
+//                                .clipShape(RoundedRectangle(cornerRadius: 10))
+//                                .overlay(
+//                                    RoundedRectangle(cornerRadius: 10)
+//                                        .stroke(.lightBackground)
+//                                )
+//                            }
+//                        }
+//                    }
+//                    .padding([.horizontal, .bottom])
+//                }
+//                .navigationTitle("Moonshot")
+//                .background(.darkBackground)
+//                .preferredColorScheme(.dark)
+//            }
+//            if showSplash {
+//                AnimatedImage(imageName: "astronaut1")
+//                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                    .background(Color.black)
+//                    .edgesIgnoringSafeArea(.all)
+//            }
+//        }
+//        .onAppear {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                withAnimation {
+//                    showSplash = false
+//                }
+//            }
+//        }
 //    }
 //}
+
+
 
 
 
